f4e1e7c5cbbd0380aef8f96965af4dfaa5b93114
Leah Boyd <leahboyd08@gmail.com>
2019-03-21 18:38:42 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,29 @@
 # Write your code here.
+katz_deli = []
+
+def take_a_number(katz_deli, name)
+  line_array = []
+  katz_deli.each.with_index(1) do |name, index|
+    line_array << puts "Welcome, #{name}. You are number #{index} in line."
+  end
+  line_array
+end
+
+def line(katz_deli)
+  index = index + 1
+  if katz_deli.size == 0
+    puts "The line is currently empty."
+  if katz_deli.size >= 1
+    puts "The line is currently: #{index}. #{name}"
+  end
+end
+
+def now_serving(katz_deli)
+  index = index + 1
+  if katz_deli.size == 0
+    puts "There is nobody waiting to be served!"
+  if katz_deli.size >= 1
+    puts "#{index}. #{name}"
+  end
+  katz_deli.delete[0]
+end
