f7e32d3d62bc2e2235b3570bb473625dab087577
Leah Boyd <leahboyd08@gmail.com>
2019-03-21 19:07:51 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -14,10 +14,11 @@ def line
   if katz_deli.size >= 1
       katz_deli.each_with_index(1) do |index, name|
         sentence_array << "The line is currently: #{index}. #{name}"
-        sentence_array
   elsif katz_deli.size == 0
-    puts "The line is currently empty."
+    "The line is currently empty."
+      end
   end
+  sentence_array
 end
 
 def now_serving(katz_deli)
