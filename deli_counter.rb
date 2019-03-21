fd8a3472a1e33d7c3de70ad64c7ba7b3c7177d60
Leah Boyd <leahboyd08@gmail.com>
2019-03-21 18:45:54 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -10,20 +10,18 @@ def take_a_number(katz_deli, name)
 end
 
 def line(katz_deli)
-  index = index + 1
   if katz_deli.size == 0
     puts "The line is currently empty."
   if katz_deli.size >= 1
-    puts "The line is currently: #{index}. #{name}"
+    puts "The line is currently: #{index+1}. #{name}"
   end
 end
 
 def now_serving(katz_deli)
-  index = index + 1
   if katz_deli.size == 0
     puts "There is nobody waiting to be served!"
   if katz_deli.size >= 1
-    puts "#{index}. #{name}"
+    puts "#{index+1}. #{name}"
   end
   katz_deli.delete[0]
 end
